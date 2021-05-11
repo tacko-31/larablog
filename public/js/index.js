594a3bd9a213714bf58c0f5b03536ff604e184ed
Tacko-31 <ricka.31@hotmail.com>
2021-05-11 14:09:50 +0000

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const searchForm = document.querySelector('#search-user');
+const searchInput = document.querySelector('#search');
+const tbody = document.querySelector('#user-list tbody');
+const BASE_URL = 'https://ricardodacostacoelho.ide.3wa.io/dev/php/m04/cours/laravel/public';
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
+
+searchInput.addEventListener('keyup', (event) => {
+    const search = searchInput.value;
+    
+    fetch(`${BASE_URL}/ajax/users?search=${search}`).then(response => response.text()).then(results => {
+        tbody.innerHTML = results;
+    });
+});
